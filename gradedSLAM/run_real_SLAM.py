a2a4107ec4978a95cb337c8c3b921c8b093398bc
Ola Rasmus Moen <ola.moen@wellstarter.com>
2020-11-10 16:20:11 +0100

    klart for vp

--- gradedSLAM/run_real_SLAM.py
+++ gradedSLAM/run_real_SLAM.py
@@ -106,15 +106,14 @@ b = 0.5  # laser distance to the left of center
 
 car = Car(L, H, a, b)
 
-sigmas = # TODO
+sigmas = [0.005, 0.005, 0.004/180*np.pi]
 CorrCoeff = np.array([[1, 0, 0], [0, 1, 0.9], [0, 0.9, 1]])
 Q = np.diag(sigmas) @ CorrCoeff @ np.diag(sigmas)
 
-R = # TODO
+R = np.diag([0.0033, 0.02/180*np.pi])
 
-JCBBalphas = np.array(
-    # TODO
-)
+JCBBalphas = np.array([5e-10, 5e-10])
+
 sensorOffset = np.array([car.a + car.L, car.b])
 doAsso = True
 
@@ -140,7 +139,7 @@ mk = mk_first
 t = timeOdo[0]
 
 # %%  run
-N = 1000#K
+N = 10000#K
 
 doPlot = False
 
@@ -176,10 +175,10 @@ for k in tqdm(range(N)):
 
         t = timeLsr[mk]  # ? reset time to this laser time for next post predict
         odo = odometry(speed[k + 1], steering[k + 1], dt, car)
-        eta, P = # TODO predict
+        eta, P = slam.predict(eta, P, odo) # TODO predict
 
         z = detectTrees(LASER[mk])
-        eta, P, NIS[mk], a[mk] = # TODO update
+        eta, P, NIS[mk], a[mk] = slam.update(eta, P, z)# TODO update
 
         num_asso = np.count_nonzero(a[mk] > -1)
 
